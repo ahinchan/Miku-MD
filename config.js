fc38777a54808cedf1d8bf9f08a7ed58d01a9ec2
ahinchan <74833102+ahinchan@users.noreply.github.com>
2022-12-23 21:14:58 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,9 +7,9 @@ global.autoReadAll = false
 global.antitags = true
 
 
-global.Owner = ['17186198364']  // -- Add multiple numbers like  ['918101967835','919357940276','917896587453']
-global.OwnerNumber = ['17186198364']  // -- Add multiple numbers like  ['918101967835','919357940276','917896587453']
-global.ownertag = ['17186198364']  // -- Add multiple numbers like  ['918101967835','919357940276','917896587453']
+global.Owner = ['7186198364']  // -- Add multiple numbers like  ['918101967835','919357940276','917896587453']
+global.OwnerNumber = ['7186198364']  // -- Add multiple numbers like  ['918101967835','919357940276','917896587453']
+global.ownertag = ['7186198364']  // -- Add multiple numbers like  ['918101967835','919357940276','917896587453']
 global.BotName = "Miku"
 global.packname = "Miku bot"
 global.author = "By: Fantox"
@@ -20,7 +20,7 @@ global.sessionName = "session"  // -- Don't change otherwise bot will not work -
 
 
 
-global.prefa = ['.'] // Prefix ---  change it to anythong you want except '@' //
+global.prefa = ['-'] // Prefix ---  change it to anythong you want except '@' //
 
 
 global.location = "West Bengal, India"
